95f70024488559b99f63ea0f5ce56d79735c2757
TheForeverOptimist <sean@elevate.app>
2023-07-18 15:08:57 -0400

    fully built

--- server.js
+++ server.js
@@ -48,10 +48,34 @@ app.post("/products", async (req, res) => {
 //updating product
 app.put("/products/:id", async (req, res) => {
   try {
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    //cannot find any product in database
+    if(!product){
+        return res.status(404).json({message: `cannot find any product with ID${id}`})
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct)
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
+
+//delete product
+
+app.delete('/products/:id', async(req,res) => {
+    try{
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        }
+        res.status(200).json(product)
+
+    } catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
 
 mongoose.set("strictQuery", false);
 mongoose
