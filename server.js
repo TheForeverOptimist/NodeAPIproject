3721eed7dfc99018198bed596374c5f81a269d5f
TheForeverOptimist <sean@elevate.app>
2023-07-18 14:20:39 -0400

    added models and multiple routes

--- server.js
+++ server.js
@@ -1,17 +1,55 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const mongoose = require('mongoose');
+const Product = require ('./models/productModel')
+const app = express();
+const port = 3000;
+
+//middleware
+app.use(express.json())
 
 //routes
 
-app.get('/', (req,res) => {
-    res.send('Hello NODE API!')
+app.get("/", (req, res) => {
+  res.send("Hello NODE API!");
+});
+
+app.get("/products", async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
 })
 
-app.get('/blog', (req, res) => {
-    res.send('Hello Blog')
+app.get("/products/:id", async(req,res) => {
+    try{
+        const products = await Product.find({});
+        res.status(200).json(products);
+    }catch (error) {
+        res.status(500).json({message: error.message})
+    }
 })
 
-app.listen(port, () => {
-    console.log(`Node API app listening on port ${port}`)
+app.post("/products", async (req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({message: error.message})
+
+    }
 })
+
+mongoose.set("strictQuery", false)
+mongoose.connect(
+  "mongodb+srv://admin:$$Laugh4$$@cluster0.p2z6py6.mongodb.net/Node-API?retryWrites=true&w=majority"
+).then(() => {
+    console.log('Connected to MongoDB')
+    app.listen(port, () => {
+  console.log(`Node API app listening on port ${port}`);
+});
+}).catch(() => {
+    console.log(error)
+});
