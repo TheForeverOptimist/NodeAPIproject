846c97d9dab15cb25595249b1f5567edec77ed0a
TheForeverOptimist <sean@elevate.app>
2023-07-18 14:49:09 -0400

    protected mongo link with dotenv nd made some overall updates

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
+
 const express = require("express");
-const mongoose = require('mongoose');
-const Product = require ('./models/productModel')
+const mongoose = require("mongoose");
+const Product = require("./models/productModel");
+const mongoDBUrl = process.env.MONGODB_URL;
 const app = express();
 const port = 3000;
 
 //middleware
-app.use(express.json())
+app.use(express.json());
 
 //routes
 
@@ -14,42 +17,51 @@ app.get("/", (req, res) => {
 });
 
 app.get("/products", async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-app.get("/products/:id", async(req,res) => {
-    try{
-        const products = await Product.find({});
-        res.status(200).json(products);
-    }catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 app.post("/products", async (req, res) => {
-    try {
-        const product = await Product.create(req.body)
-        res.status(200).json(product);
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).json({message: error.message})
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
 
-    }
-})
+//updating product
+app.put("/products/:id", async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-mongoose.set("strictQuery", false)
-mongoose.connect(
-  "mongodb+srv://admin:$$Laugh4$$@cluster0.p2z6py6.mongodb.net/Node-API?retryWrites=true&w=majority"
-).then(() => {
-    console.log('Connected to MongoDB')
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(mongoDBUrl)
+  .then(() => {
+    console.log("Connected to MongoDB");
     app.listen(port, () => {
-  console.log(`Node API app listening on port ${port}`);
-});
-}).catch(() => {
-    console.log(error)
-});
+      console.log(`Node API app listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
